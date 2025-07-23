e30d34a183668521a25824a5292ad9e41457d123
Sithija R <Sithijarathukorale@gmail.com>
2025-07-23 16:56:01 +0530

    why choose us

---
+++ components/ContactForm.tsx
@@ -0,0 +1,10 @@
+'use client';
+import React from 'react'
+
+const ContactForm = () => {
+  return (
+    <div className='bg-[rgba(255,255,255,0.04)]'>ContactForm</div>
+  )
+}
+
+export default ContactForm
